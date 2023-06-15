1e647c9e2f9367171116c460e2e24eb6f109c789
oluwaseun-idris <oluwaseunidris2004@gmail.com>
2023-06-15 15:27:26 -0700

    Printing standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
  * Return: Always 1 (error code)
  */
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = 59;
 
-	write(2, message, 59);
+	write(STDERR_FILENO, message, len);
+
 	return (1);
 }
-
