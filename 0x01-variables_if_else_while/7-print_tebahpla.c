98464f4f59ab821af3605a6d772a56bfe55112f5
oluwaseun-idris <oluwaseunidris2004@gmail.com>
2023-06-16 12:03:54 -0700

    printing tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point program for alphabet
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
