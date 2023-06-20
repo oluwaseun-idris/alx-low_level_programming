48293d65e1559ffb7c9f8d4a3f97c828252693c8
oluwaseun-idris <oluwaseunidris2004@gmail.com>
2023-06-20 11:00:50 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry program that prints alphabets
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
