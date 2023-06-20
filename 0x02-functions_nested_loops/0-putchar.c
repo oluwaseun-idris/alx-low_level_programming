548510dc33bfeb6ed8fad4bcb63e7c4ac3ea5893
oluwaseun-idris <oluwaseunidris2004@gmail.com>
2023-06-20 07:54:59 -0700

    Printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry program that prints a word
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
