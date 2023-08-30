fb5703a0f3b16a5be76ae4279766491013953a10
oluwaseun-idris <oluwaseunidris2004@gmail.com>
2023-08-30 09:30:54 -0700

    6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * is_divisible - Checks if a number is divisible by any smaller number.
+ * @n: The number to check.
+ * @divisor: The divisor to check.
+ *
+ * Return: 1 if n is divisible by divisor, 0 otherwise.
+ */
+int is_divisible(int n, int divisor)
+{
+    if (divisor == 1)
+    {
+        return (0);
+    }
+
+    if (n % divisor == 0)
+    {
+        return (1);
+    }
+
+    return (is_divisible(n, divisor - 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is a prime number.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+    if (n <= 1)
+    {
+        return (0);
+    }
+
+    return (!is_divisible(n, n - 1));
+}
+
