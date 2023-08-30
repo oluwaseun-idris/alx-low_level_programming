364dde2012bca54d317b4d8caea6c66ef68d5d47
oluwaseun-idris <oluwaseunidris2004@gmail.com>
2023-08-30 07:40:59 -0700

    2. Dreams feel real while we're in them.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _strlen_recursion - A function that returns the length of a string
  * @s: Pointer to the string to be printed
- * return - 0
+ * return: Success
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
